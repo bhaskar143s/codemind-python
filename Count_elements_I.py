347e8296e87aa8e8f9e36e314773684f05850db0
bhaskar143s <106300895+bhaskar143s@users.noreply.github.com>
2022-06-30 18:54:35 +0530

    a,b=map(int,input().split()) arr=list(map(int,input().split())) arr2=list(map(int,input().split())) x=0 for i in range(a):     for j in range(a):         if i!=j:             if arr[i]==arr[j]:                  if arr[i]!=-1000:                      arr[j]=-1000 for i in range(b):     for j in range(b):         if i!=j:             if arr2[i]==arr2[j] and arr2[i]!=-100:                 arr2[j]=-100 for i in arr:     if i!=-1:         if i in arr2:             x+=1 for i in arr2:     if i!=-1:         if i in arr:             x+=1 print(x//2)

---
+++ Count_elements_I.py
@@ -0,0 +1,29 @@
+a,b=map(int,input().split())
+arr=list(map(int,input().split()))
+arr2=list(map(int,input().split()))
+x=0
+for i in range(a):
+    for j in range(a):
+        if i!=j:
+            if arr[i]==arr[j]:
+                 if arr[i]!=-1000:
+                     arr[j]=-1000
+for i in range(b):
+    for j in range(b):
+        if i!=j:
+            if arr2[i]==arr2[j] and arr2[i]!=-100:
+                arr2[j]=-100
+for i in arr:
+    if i!=-1:
+        if i in arr2:
+            x+=1
+for i in arr2:
+    if i!=-1:
+        if i in arr:
+            x+=1
+print(x//2)
+
+
+
+
+

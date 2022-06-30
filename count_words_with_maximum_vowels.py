066f8e8bc3a1ba5bae9ab11290b20f8fb5b3e177
bhaskar143s <106300895+bhaskar143s@users.noreply.github.com>
2022-06-30 19:54:53 +0530

    n=input() z=n.split() q="aeiouAEIOU" ma=x=0 for i in z:     c=0     for j in i:         if j in q:             c+=1     if ma<c:         ma=c for i in z:     c=0     for j in i:         if j in q:             c+=1     if ma==c:         x+=1 print(x)

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,20 @@
+n=input()
+z=n.split()
+q="aeiouAEIOU"
+ma=x=0
+for i in z:
+    c=0
+    for j in i:
+        if j in q:
+            c+=1
+    if ma<c:
+        ma=c
+for i in z:
+    c=0
+    for j in i:
+        if j in q:
+            c+=1
+    if ma==c:
+        x+=1
+print(x)
+    

695cb139dc4fa3ae8401913008fd11f14f180a7a
bhaskar143s <106300895+bhaskar143s@users.noreply.github.com>
2022-10-16 12:24:58 +0530

    n,m=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) c=[] #a=set(a) #b=set(b) for i in a:     if i not in b:         c.append(i) for i in b:     if i not in a:         c.append(i) print(*c)

--- Common_elements_II.py
+++ Common_elements_II.py
@@ -1,25 +1,15 @@
-a,b=map(int,input().split())
-arr=list(map(int,input().split()))
-arr2=list(map(int,input().split()))
-x=0
-for i in range(a):
-    for j in range(a):
-        if i!=j:
-            if arr[i]==arr[j]:
-                 if arr[i]!=-1000:
-                     arr[j]=-1000
-for i in range(b):
-    for j in range(b):
-        if i!=j:
-            if arr2[i]==arr2[j]:
-                if arr2[i]!=-100:
-                    arr2[j]=-100
-for i in arr:
-    if i!=-1000:
-        if i  not in arr2:
-            print(i,end=" ")
-for i in arr2:
-    if i!=-100:
-        if i not in arr:
-           print(i,end=" ")
-#print(x)
+n,m=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+c=[]
+#a=set(a)
+#b=set(b)
+for i in a:
+    if i not in b:
+        c.append(i)
+for i in b:
+    if i not in a:
+        c.append(i)        
+print(*c)        
+    
+

3b79ca9bbc43ddb5a45b130e8163a66fb8a6b3c3
bhaskar143s <106300895+bhaskar143s@users.noreply.github.com>
2022-10-16 12:16:43 +0530

    def fun(n):     s=0     while(n>0):         r=n%10         s=s+r         n//=10     return s
    n=int(input())
    a=list(map(int,input().split()))
    s=0
    for i in a:
        c=fun(i)
        s=s+c
    print(s)

---
+++ Play_with_digits.py
@@ -0,0 +1,17 @@
+def fun(n):
+    s=0
+    while(n>0):
+        r=n%10
+        s=s+r
+        n//=10
+    return s
+        
+n=int(input())
+a=list(map(int,input().split()))
+s=0
+for i in a:
+    c=fun(i)
+    s=s+c
+print(s)
+
+

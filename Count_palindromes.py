a062fe3b8b8315f69817aca02729bcb8b790bb5a
bhaskar143s <106300895+bhaskar143s@users.noreply.github.com>
2022-10-16 12:13:27 +0530

    def fun(n):     s=0     t=n     while(n):         s=s*10+(n%10)         n//=10     if s==t:         return True     else:         return False n=int(input()) a=list(map(int,input().split()[:n])) c=0 for i in a:     if fun(i):         c+=1 print(c)

---
+++ Count_palindromes.py
@@ -0,0 +1,18 @@
+def fun(n):
+    s=0
+    t=n
+    while(n):
+        s=s*10+(n%10)
+        n//=10
+    if s==t:
+        return True
+    else:
+        return False
+n=int(input())
+a=list(map(int,input().split()[:n]))
+c=0
+for i in a:
+    if fun(i):
+        c+=1
+print(c)
+

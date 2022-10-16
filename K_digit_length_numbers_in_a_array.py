72e56f2b207ef52c40b39030887716f01e83da6c
bhaskar143s <106300895+bhaskar143s@users.noreply.github.com>
2022-10-16 12:07:52 +0530

    def fun(n):     c=0     if n<0:         n=n*(-1)     if n==0:         return 1     while(n):         r=n%10         c+=1         n//=10     return c m,n=map(int,input().split()) a=list(map(int,input().split())) c=0 for i in a:     k=fun(i)     if k==n:         c+=1 print(c)

---
+++ K_digit_length_numbers_in_a_array.py
@@ -0,0 +1,20 @@
+def fun(n):
+    c=0
+    if n<0:
+        n=n*(-1)
+    if n==0:
+        return 1
+    while(n):
+        r=n%10
+        c+=1
+        n//=10
+    return c
+m,n=map(int,input().split())
+a=list(map(int,input().split()))
+c=0
+for i in a:
+    k=fun(i)
+    if k==n:
+        c+=1
+print(c)
+
